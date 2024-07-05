944178fe4a83dbbc45abc00ea4973f787506300b
Peterson <peterson.silva6@fatec.sp.gov.br>
2024-07-05 14:39:05 -0300

    with error

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,30 +1,30 @@
-﻿namespace CatalogAPI.Products.CreateProduct;
-    
-    public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+﻿namespace Catalog.API.Products.CreateProduct;
 
-    public record CreateProductResponse(Guid Id);
+public record CreateProductRequest(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
 
-public class CreateProductEndPoint : ICarterModule
+public record CreateProductResponse(Guid Id);
+
+public class CreateProductEndpoint : ICarterModule
 {
     public void AddRoutes(IEndpointRouteBuilder app)
     {
         app.MapPost("/products",
             async (CreateProductRequest request, ISender sender) =>
-        {
-            var command = request.Adapt<CreateProductCommand>();
+            {
+                var command = request.Adapt<CreateProductCommand>();
 
-            var result = await sender.Send(command);
+                var result = await sender.Send(command);
 
-            var response = result.Adapt<CreateProductResponse>();
+                var response = result.Adapt<CreateProductResponse>();
 
-            return Results.Created($"/products/{response.Id}", response);
-        })
+                return Results.Created($"/products/{response.Id}", response);
+
+            })
         .WithName("CreateProduct")
         .Produces<CreateProductResponse>(StatusCodes.Status201Created)
         .ProducesProblem(StatusCodes.Status400BadRequest)
         .WithSummary("Create Product")
         .WithDescription("Create Product");
-        
     }
 }
 
