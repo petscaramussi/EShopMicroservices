0485aef5c730170e8de063f77d1bf403a86b256f
Peterson <peterson.silva6@fatec.sp.gov.br>
2024-08-01 09:49:12 -0300

    aula 72

---
+++ src/Services/Catalog/CatalogAPI/Exceptions/ProductNotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace CatalogAPI.Exceptions;
+
+public class ProductNotFoundException : Exception
+{
+    public ProductNotFoundException() : base("Product not found!")
+    {
+    }
+}
+
--- src/Services/Catalog/CatalogAPI/GlobalUsing.cs
+++ src/Services/Catalog/CatalogAPI/GlobalUsing.cs
@@ -4,4 +4,4 @@ global using MediatR;
 global using BuildingBlocks.CQRS;
 global using CatalogAPI.Models;
 global using Marten;
-
+global using CatalogAPI.Exceptions;
